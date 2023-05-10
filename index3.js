cf6bccb96272ceb1210793753d6a6ed7386aaa5c
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-10 14:45:45 +0700

    x

--- index3.js
+++ index3.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 const news = [
     {
-        "id": "1",
-        "description": "Trung Quốc điều thêm 17 tàu đến khu vực giàn khoan",
-        "detail": "Để bảo vệ giàn khoan, Trung Quốc đã điều thêm 17 tàu các loại so với hôm trước, sẵn sàng đâm va vào tàu Việt Nam.",
-        "catId": "1"
+        id: "1",
+        description: "Trung Quốc điều thêm 17 tàu đến khu vực giàn khoan",
+        detail: "Để bảo vệ giàn khoan, Trung Quốc đã điều thêm 17 tàu các loại so với hôm trước, sẵn sàng đâm va vào tàu Việt Nam.",
+        catId: "1"
     },
     {
-        "id": "2",
-        "description": "Trọng tài - vết đen của kỳ World Cup sôi động",
-        "detail": "World Cup 2014 chưa đi hết lượt đầu vòng bảng nhưng các trọng tài đẳng cấp FIFA đã có tới bốn trận bị chỉ trích dữ dội.",
-        "catId": "2"
+        id: "2",
+        description: "Trọng tài - vết đen của kỳ World Cup sôi động",
+        detail: "World Cup 2014 chưa đi hết lượt đầu vòng bảng nhưng các trọng tài đẳng cấp FIFA đã có tới bốn trận bị chỉ trích dữ dội.",
+        catId: "2"
     },
     {
-        "id": "3",
-        "description": "Những mỹ nhân Việt duyên dáng ở tuổi tứ tuần",
-        "detail": "Việt Trinh, Thu Hà, Hồng Nhung, Thanh Lam... vẫn giữ được nét thanh xuân, tươi trẻ và cuốn hút theo thời gian nhờ phong cách làm đẹp tinh tế.",
-        "catId": "1"
+        id: "3",
+        description: "Những mỹ nhân Việt duyên dáng ở tuổi tứ tuần",
+        detail: "Việt Trinh, Thu Hà, Hồng Nhung, Thanh Lam... vẫn giữ được nét thanh xuân, tươi trẻ và cuốn hút theo thời gian nhờ phong cách làm đẹp tinh tế.",
+        catId: "1"
     },
     {
-        "id": "4",
-        "description": "Chuyển nhượng 17/6: Arsenal mua Balotelli, tráo hàng Man Utd",
-        "detail": "Đội bóng thành London đang đẩy mạnh việc tuyển lựa những vị trí còn yếu trong đội hình ở cả ba tuyến.",
-        "catId": "3"
+        id: "4",
+        description: "Chuyển nhượng 17/6: Arsenal mua Balotelli, tráo hàng Man Utd",
+        detail: "Đội bóng thành London đang đẩy mạnh việc tuyển lựa những vị trí còn yếu trong đội hình ở cả ba tuyến.",
+        catId: "3"
     },
     {
-        "id": "5",
-        "description": "Chuyên gia Anh tin chắc vị trí MH370 rơi",
-        "detail": "Các chuyên gia thuộc công ty viễn thông Anh Inmarsat cho rằng họ xác định được vị trí chiếc máy bay MH370 đã rơi xuống trên Ấn Độ Dương, tuy nhiên đội.",
-        "catId": "1"
+        id: "5",
+        description: "Chuyên gia Anh tin chắc vị trí MH370 rơi",
+        detail: "Các chuyên gia thuộc công ty viễn thông Anh Inmarsat cho rằng họ xác định được vị trí chiếc máy bay MH370 đã rơi xuống trên Ấn Độ Dương, tuy nhiên đội.",
+        catId: "1"
     }
 ]
 
@@ -74,7 +74,18 @@ app.get('/anewsbycat', (req, res) => {
 })
 
 app.get('/anewsbyid', (req, res) => {
+    console.log(111);
     var dId = req.query.did;
+
+    var newsDetail = news.find(function (st) {
+        return st.id === dId;
+    })
+    res.send(newsDetail);
+})
+
+app.get('/anewsbyid/:id', (req, res) => {
+    console.log(222);
+    var dId = req.params.id;
 
     var newsDetail = news.find(function (st) {
         return st.id === dId;
