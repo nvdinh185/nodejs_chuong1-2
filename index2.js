7b84debcaf54a12da1fe6d0163ec8dea2fa0db02
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-16 17:20:01 +0700

    c

--- index2.js
+++ index2.js
@@ -4,41 +4,41 @@ const port = 3000;
 
 const students = [
     {
-        id: 1,
+        id: '1',
         name: "Dinh",
         address: "hue"
     },
     {
-        id: 2,
+        id: '2',
         name: "Nam",
         address: "quang nam"
     },
     {
-        id: 3,
+        id: '3',
         name: "Tan",
         address: "da nang"
     },
     {
-        id: 4,
+        id: '4',
         name: "Hung",
         address: "hue"
     },
     {
-        id: 5,
+        id: '5',
         name: "Tri",
         address: "quang tri"
     },
     {
-        id: 6,
+        id: '6',
         name: "Anh",
         address: "hue"
     },
     {
-        id: 7,
+        id: '7',
         name: "Binh",
         address: "da nang"
     }
-];
+]
 
 app.get('/students', (req, res) => {
     res.send(students);
@@ -51,6 +51,15 @@ app.get('/studentsbyaddress', (req, res) => {
         return st.address === address;
     })
     res.send(newStudents);
+})
+
+app.get('/studentbyid', (req, res) => {
+    var id = req.query.id;
+
+    var newStudent = students.find(function (st) {
+        return st.id === id;
+    })
+    res.send(newStudent);
 })
 
 app.listen(port, () => {
