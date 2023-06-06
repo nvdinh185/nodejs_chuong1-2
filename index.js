d07a2268111f55b96f2803a25b300f7dd5644fa5
nvdinh185 <nvdinh0766777123@gmail.com>
2023-06-06 10:28:45 +0700

    users

--- index.js
+++ index.js
@@ -2,17 +2,60 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+
+const users = [
+    {
+        username: 'dinh',
+        password: '123',
+        fullname: 'Nguyen Van Dinh'
+    }
+]
+
+app.use(express.static(__dirname + '/client'));
+
+app.post('/register', (req, res) => {
+    var user = req.body;
+    var check = false;
+
+    for (const el of users) {
+        if (user.username === el.username) {
+            check = true;
+        }
+    }
+
+    if (!check) {
+        users.push(user);
+        res.status(200).send({ status: 'OK' });
+    } else {
+        res.status(500).send({ status: 'NOK' });
+    }
 })
 
-app.get('/home', (req, res) => {
-    res.send(
-        {
-            id: 1,
-            name: "hello"
+app.post('/login', (req, res) => {
+    var user = req.body;
+    var check = false;
+
+    for (const el of users) {
+        if (user.username === el.username
+            && user.password === el.password) {
+            check = true;
         }
-    );
+    }
+
+    if (check) {
+        res.status(200).send({ status: 'OK' });
+    } else {
+        res.status(500).send({ status: 'NOK' });
+    }
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
 })
 
 app.listen(port, () => {
