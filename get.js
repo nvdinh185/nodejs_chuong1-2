dd8ca0c5d2b964062f65dbdc74c04c38104a8de8
nvdinh185 <nvdinh0766777123@gmail.com>
2023-09-11 08:17:33 +0700

    x

--- get.js
+++ get.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static(__dirname + '/client'));
 
 app.get('/search', function (req, res) {
     console.log('GET: ', req.query);
-    res.send('search');
+    res.send('search ok');
+})
+
+app.post('/register', function (req, res) {
+    console.log('POST: ', req.body);
+    res.send('register ok');
 })
 
 app.listen(port, () => {
