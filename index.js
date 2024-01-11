f78570a416105c54b6c8aa69930ba67d8f57ec34
nvdinh185 <nvdinh0766777123@gmail.com>
2024-01-11 09:09:52 +0700

    x

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+// Khai báo đường dẫn cho các file tĩnh
+app.use(express.static(__dirname + '/client'));
+
+// Khai báo các router
+app.get('/hello', (req, res) => {
     res.send('Hello World!');
 })
 
@@ -10,11 +14,16 @@ app.get('/home', (req, res) => {
     res.send(
         {
             id: 1,
-            name: "hello"
+            name: "home"
         }
     );
 })
 
+app.get('/news', (req, res) => {
+    res.sendFile(__dirname + '/client/tintuc.html');
+})
+
+// Khởi chạy server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
